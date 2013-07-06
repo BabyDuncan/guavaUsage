884ee281799ef6de93c2f5e9afc53c657e2929fe
guohaozhao <guohaozhao116008@sohu-inc.com>
2013-07-06 21:48:14 +0800

    init project

---
+++ src/main/java/com/babyduncan/guavaUsage/basic/PreconditionsUsage.java
@@ -0,0 +1,18 @@
+package com.babyduncan.guavaUsage.basic;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * User: zgh
+ * Date: 13-2-24
+ * Time: 17:00
+ */
+public class PreconditionsUsage {
+    public static void main(String... args) {
+//        int i = -1;
+//        checkArgument(i >= 0, "Argument was %s but expected nonnegative", i);
+//        System.out.println("test");
+        String s = null;
+        System.out.println(checkNotNull(s, "not null", "fuck"));
+    }
+}
